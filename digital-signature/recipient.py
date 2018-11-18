67d4478325a0eb14cf9f68ee22b4a28a6c38e88a
Gajuli <gusevyuri359@gmail.com>
2018-11-18 20:25:42 +0300

    :wq

--- digital-signature/recipient.py
+++ digital-signature/recipient.py
@@ -1,1 +1,15 @@
 import libcrypt
+
+f = open("message.txt", "r")
+s=f.read()
+a=s.split('\n')
+e=int(a[0])
+n=int(a[1])
+m=int(a[2])
+s=int(a[3])
+decr_m=libcrypt.expmod(s,e,n)
+if decr_m==m:
+    print('ok')
+else:
+    print('not ok')
+f.close()
