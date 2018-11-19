8925c092e37c5f47af798b75cd45c7cc6fa6dcd4
Vsevolod Sharov <vsevolod.sharov@gmail.com>
2018-11-19 19:11:37 +0300

    Sender and recipient implement client-server model now

--- digital-signature/recipient.py
+++ digital-signature/recipient.py
@@ -1,15 +1,35 @@
-import libcrypt
+import socket
 
-f = open("message.txt", "r")
-s=f.read()
-a=s.split('\n')
-e=int(a[0])
-n=int(a[1])
-m=int(a[2])
-s=int(a[3])
-decr_m=libcrypt.expmod(s,e,n)
-if decr_m==m:
-    print('ok')
-else:
-    print('not ok')
-f.close()
+import libcrypt
+import connection as conn
+
+def main():
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.bind((conn.HOST, conn.PORT))
+	sock.listen(conn.NCLIENTS)
+
+	while True:
+		client_connection, client_address = sock.accept()
+		print("Connected client: " + str(client_address))
+		client_data = client_connection.recv(conn.NBYTES)
+		print("Received data:\n" + client_data.decode())
+		ack = check_signature(client_data.decode())
+		client_connection.send(bytes(ack, "UTF-8"))
+		client_connection.close()
+		
+	sock.close()
+
+def check_signature(data):
+	arr = data.split('\n')
+	e = int(arr[0])
+	n = int(arr[1])
+	m = int(arr[2])
+	s = int(arr[3])
+	decr_m=libcrypt.expmod(s,e,n)
+	if decr_m == m:
+		return "ok"
+	else:
+		return "not ok"
+
+if __name__ == "__main__":
+	main()
--- digital-signature/sender.py
+++ digital-signature/sender.py
@@ -1,42 +1,54 @@
+import socket
+
 import libcrypt
+import connection as conn
 
-max_num = int(1e+9)
-max_test = 100
+def main():
+	parcel = create_signature(503)
+	
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.connect((conn.HOST, conn.PORT))
+	sock.send(bytes(parcel, "UTF-8"))
 
-message = 1488
+	data = sock.recv(conn.NBYTES)
+	print(data.decode())
 
-# Выбираем 2 больших простых числа
-p = libcrypt.find_primes(max_num, max_test, 1)[0]
-q = libcrypt.find_primes(2 * max_num, max_test, 1)[0]
+	sock.close()
 
-# Вычисляем модуль открытого ключа
-n = p * q
+def create_signature(message):
+	max_num = int(1e+9)
+	max_test = 100
 
-# Вычисляем функцию Эйлера от n,
-# используя свойство для взаимно простых чисел
-euler_f = (p-1) * (q-1)
+	# Выбираем 2 больших простых числа
+	p = libcrypt.find_primes(max_num, max_test, 1)[0]
+	q = libcrypt.find_primes(2 * max_num, max_test, 1)[0]
 
-# Выбираем целое число e из диапазона [1, euler_f] такое,
-# что e и euler_f должны быть взаимно простыми
-# Для простоты выбираем одно из чисел Ферма
-if euler_f < 10:
-	e = libcrypt.simple_fermat_nums[0]
-elif euler_f < 100:
-	e = libcrypt.simple_fermat_nums[1]
-elif euler_f < 100000:
-	e = libcrypt.simple_fermat_nums[2]
-else:
-	e = libcrypt.simple_fermat_nums[3]
+	# Вычисляем модуль открытого ключа
+	n = p * q
 
-# Вычисляем закрытый ключ d
-d = abs(libcrypt.expanded_gcd(e, euler_f)[1]) 
+	# Вычисляем функцию Эйлера от n,
+	# используя свойство для взаимно простых чисел
+	euler_f = (p-1) * (q-1)
 
-#  Шифруем сообщение message
-signature = libcrypt.expmod(message, d, n)
+	# Выбираем целое число e из диапазона [1, euler_f] такое,
+	# что e и euler_f должны быть взаимно простыми
+	# Для простоты выбираем одно из чисел Ферма
+	if euler_f < 10:
+		e = libcrypt.simple_fermat_nums[0]
+	elif euler_f < 100:
+		e = libcrypt.simple_fermat_nums[1]
+	elif euler_f < 100000:
+		e = libcrypt.simple_fermat_nums[2]
+	else:
+		e = libcrypt.simple_fermat_nums[3]
 
-f = open("message.txt", "w")
-f.write(str(e) + '\n' +
-		str(n) + '\n' +
-		str(message) + '\n' +
-		str(signature) + '\n')
-f.close()
+	# Вычисляем закрытый ключ d
+	d = abs(libcrypt.expanded_gcd(e, euler_f)[1]) 
+
+	#  Шифруем сообщение message
+	signature = libcrypt.expmod(message, d, n)
+
+	return str(e) + '\n' + str(n) + '\n' + str(message) + '\n' + str(signature) + '\n'
+	
+if __name__ == "__main__":
+	main()
